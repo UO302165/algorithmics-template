f3e5e5383dcf9c1f79bffd0b5ef44dfde396d09a
Lucas Uña García <UO302165@uniovi.es>
2025-02-13 19:54:22 +0100

    session1.2

---
+++ algstudent/s12/Loop5.java
@@ -0,0 +1,36 @@
+package algorithmicsS2;
+
+public class Loop5 {
+
+	public static long loop4(int n) {
+		long cont = 0;
+		for (int i = 1; i <= n*n; i++)
+			for (int j = 1; j <= n; j*=2)
+				for (int k = 1; k <= n; k*=2)
+					cont++;
+		return cont;
+
+	}
+
+	public static void main(String arg[]) {
+		long c = 0;
+		long t1, t2;
+
+		int nTimes = Integer.parseInt(arg[0]);
+
+		System.out.println("n\ttime\trepetions\tcounter");
+
+		for (int n = 100; n <= 819200; n *= 2) {
+			t1 = System.currentTimeMillis();
+
+			for (int repetitions = 1; repetitions <= nTimes; repetitions++)
+				c = loop4(n);
+
+			t2 = System.currentTimeMillis();
+
+			System.out.println(n + "\t" + (t2 - t1) + "\t" + nTimes + "\t\t" + c);
+		} // for
+	} // main
+
+
+}
